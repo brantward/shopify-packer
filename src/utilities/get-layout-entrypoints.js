50a979965834a5f356f4229e32697b7b66fc1063
Brant <brant@thesynergyeffect.com>
2021-09-03 15:49:03 +0800

    Update get-layout-entrypoints.js

--- src/utilities/get-layout-entrypoints.js
+++ src/utilities/get-layout-entrypoints.js
@@ -7,18 +7,20 @@ const config = new PackerConfig(require('../../packer.schema'));
 module.exports = function () {
   const entrypoints = {};
 
-  fs.readdirSync(config.get('theme.src.layout')).forEach((file) => {
-    const name = path.parse(file).name;
-    if (!name.includes('.critical')) {
-      const jsFile = path.join(
-        config.get('theme.src.scripts'),
-        'layout',
-        `${name}.js`
-      );
-      if (fs.existsSync(jsFile)) {
-        entrypoints[`layout.${name}`] = jsFile;
+  fs.readdirSync(path.join(config.get('theme.src.scripts'), 'layout')).forEach(
+    (file) => {
+      const name = path.parse(file).name;
+      if (!name.includes('.critical')) {
+        const jsFile = path.join(
+          config.get('theme.src.scripts'),
+          'layout',
+          `${name}.js`
+        );
+        if (fs.existsSync(jsFile)) {
+          entrypoints[`layout.${name}`] = jsFile;
+        }
       }
     }
-  });
+  );
   return entrypoints;
 };
